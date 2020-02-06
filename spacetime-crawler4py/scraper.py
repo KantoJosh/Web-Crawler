948c60e62d59bf1f560595c0af8169b7eb5d0ad9
Andy Huynh Kha <ahkha@circinus-6.ics.uci.edu>
2020-02-06 13:35:16 -0800

    Added dict to track visited pages. Added pdf to regex

--- spacetime-crawler4py/scraper.py
+++ spacetime-crawler4py/scraper.py
@@ -1,8 +1,9 @@
 import re
-
-
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin, urldefrag
+from collections import defaultdict
+
+visited = defaultdict(int)
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -19,21 +20,21 @@ def extract_next_links(url, resp):
     for a in soup.find_all('a', href = True):
         #print("before=",a['href'],"after=",urljoin(url,a['href']))
         abs_url = urldefrag(urljoin(url,a['href']))[0]
-        if(is_valid(abs_url)):
+        if(is_valid(abs_url) and abs_url not in visited):
+            visited[abs_url] = 0
             links.append(abs_url)
     return links
 
 def is_valid(url):
     try:
         parsed = urlparse(url)
-        print("PATH",url,parsed,parsed.path)
         if parsed.scheme not in set(["http", "https"]):
             return False
         return re.match(r"(.*(\.ics\.uci\.edu\/*|\.cs\.uci\.edu\/|\.informatics\.uci\.edu\/|\.stat\.uci\.edu\/|today\.uci\.edu\/department\/information_computer_sciences).*)",
-                        url) and not re.match(r".*replytocom.*",parsed.query.lower()) and not re.match(
+                        url) and not re.match(r".*replytocom.*|.*pdf.*",url) and not re.match(
                 r".*\.(css|js|bmp|gif|jpe?g|ico"
                 + r"|png|tiff?|mid|mp2|mp3|mp4"
-                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|ppsx"
                 + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
                 + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
                 + r"|epub|dll|cnf|tgz|sha1"
