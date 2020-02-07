f8aa9f34a858a44630154288c0cba59e614b752d
Andy Huynh Kha <ahkha@circinus-2.ics.uci.edu>
2020-02-07 12:09:38 -0800

    Small changes to dictionary

--- spacetime-crawler4py/scraper.py
+++ spacetime-crawler4py/scraper.py
@@ -20,19 +20,30 @@ def extract_next_links(url, resp):
     for a in soup.find_all('a', href = True):
         #print("before=",a['href'],"after=",urljoin(url,a['href']))
         abs_url = urldefrag(urljoin(url,a['href']))[0]
-        if(is_valid(abs_url) and abs_url not in visited):
-            visited[abs_url] = 0
-            links.append(abs_url)
+        if(is_valid(abs_url)):
+            parsed = urlparse(abs_url)
+            check_duplicate = parsed.netloc.lower() + parsed.path.lower() + parsed.params.lower() + parsed.query.lower()
+            if(check_duplicate not in visited):
+                visited[check_duplicate] = 0
+                links.append(abs_url)
     return links
 
 def is_valid(url):
     try:
         parsed = urlparse(url)
+        path_params = parsed.path.lower()
+        
+        #Check to see that path params arent repeating, i.e, stayconnected/stayconnected/stayconnected/stayconnected
+        if(len(path_params) > 1):
+            for i in range(len(path_params)-1):
+                if(path_params[i] == path_params[i+1]):
+                    return False
+
         if parsed.scheme not in set(["http", "https"]):
             return False
         return re.match(r"(.*(\.ics\.uci\.edu\/*|\.cs\.uci\.edu\/|\.informatics\.uci\.edu\/|\.stat\.uci\.edu\/|today\.uci\.edu\/department\/information_computer_sciences).*)",
                         url) and not re.match(r".*replytocom.*|.*pdf.*",url) and not re.match(
-                r".*\.(css|js|bmp|gif|jpe?g|ico"
+                r".*(css|js|bmp|gif|jpe?g|ico"
                 + r"|png|tiff?|mid|mp2|mp3|mp4"
                 + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|ppsx"
                 + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
