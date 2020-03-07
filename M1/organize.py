e069c28e390f7b4137d5c99b8d503dbd5f556813
anha97 <45274798+anha97@users.noreply.github.com>
2020-03-06 18:36:20 -0800

    tf-idf score added

--- M1/organize.py
+++ M1/organize.py
@@ -1,4 +1,5 @@
 import pickle
+import math
 from indexer import create_index, InvertedIndex
 def main():
     i = 0
@@ -16,15 +17,28 @@ def main():
     #for key, value in final_result.getDict().items():
     #   for key2, value2 in value.items():
     #        print(key, key2, value2)
-    exit()
+
+    tf-score = defaultdict()
+    N = len(final_result.getDict().keys())   # Total number of documents/urls
+    for key, value in final_result.getDict().items():   # key = "token" & value = dictionary
+        df = len(value.keys()) # Get the number of documents/urls that contain "token"
+        for key2, value2 in value.items():  # key2 = docID & value2 = tf
+            tf-score[key][key2] = value2 * math.log(N/df, 10)   # tf * log(N/df); tf ==> (1 + log(tf) in indexer.py)
     # Creating separate dictionaries for each alphabet
+    for i in tf-score:
+        if 97 <= ord(i[0]) <= 122: # if the key of the index is part of english alphabet
+            alphanumIndex[ord(i[0]) - 97][i] = tf-score[i]    # the first index selects which partial dictionary the key belongs to
+                                                                            # the second index maps the key i to the value of the key i from the merged index
+        else:
+            alphanumIndex[26][i] = tf-score[i]                # the key is not part of eng. alphabet -> insert in number index
+    '''
     for i in final_result.getDict():
         if 97 <= ord(i[0]) <= 122: # if the key of the index is part of english alphabet
             alphanumIndex[ord(i[0]) - 97][i] = final_result.getDict()[i]    # the first index selects which partial dictionary the key belongs to
                                                                             # the second index maps the key i to the value of the key i from the merged index
         else:
             alphanumIndex[26][i] = final_result.getDict()[i]                # the key is not part of eng. alphabet -> insert in number index
-
+    '''
 
     ## create partial file for each letter and dump the dict into that file
     ASCII_code = 97 # ascii of 'a'
