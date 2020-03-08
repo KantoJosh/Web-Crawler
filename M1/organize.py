e2ed476efe698cb4fd49762311980ac962f39050
anha97 <45274798+anha97@users.noreply.github.com>
2020-03-07 19:53:09 -0800

    Add files via upload

--- M1/organize.py
+++ M1/organize.py
@@ -1,5 +1,6 @@
 import pickle
 import math
+from collections import defaultdict
 from indexer import create_index, InvertedIndex
 def main():
     i = 0
@@ -18,19 +19,19 @@ def main():
     #   for key2, value2 in value.items():
     #        print(key, key2, value2)
 
-    tf-score = defaultdict()
+    tf_score = defaultdict()
     N = len(final_result.getDict().keys())   # Total number of documents/urls
     for key, value in final_result.getDict().items():   # key = "token" & value = dictionary
         df = len(value.keys()) # Get the number of documents/urls that contain "token"
         for key2, value2 in value.items():  # key2 = docID & value2 = tf
-            tf-score[key][key2] = value2 * math.log(N/df, 10)   # tf * log(N/df); tf ==> (1 + log(tf) in indexer.py)
+            tf_score[key][key2] = value2 * math.log(N/df, 10)   # tf * log(N/df); tf ==> (1 + log(tf) in indexer.py)
     # Creating separate dictionaries for each alphabet
-    for i in tf-score:
+    for i in tf-score:  # i is the "token" & tf_score[i] is the dictionary containing {docID:tf-idf}
         if 97 <= ord(i[0]) <= 122: # if the key of the index is part of english alphabet
-            alphanumIndex[ord(i[0]) - 97][i] = tf-score[i]    # the first index selects which partial dictionary the key belongs to
+            alphanumIndex[ord(i[0]) - 97][i] = tf_score[i]    # the first index selects which partial dictionary the key belongs to
                                                                             # the second index maps the key i to the value of the key i from the merged index
         else:
-            alphanumIndex[26][i] = tf-score[i]                # the key is not part of eng. alphabet -> insert in number index
+            alphanumIndex[26][i] = tf_score[i]                # the key is not part of eng. alphabet -> insert in number index
     '''
     for i in final_result.getDict():
         if 97 <= ord(i[0]) <= 122: # if the key of the index is part of english alphabet
